6b4702720e8738b1aa0fd31bf359e03ae40985fd
mathis150 <48561648+mathis150@users.noreply.github.com>
2026-02-16 21:21:30 +0100

    💄 Make navigations classes in general style

--- assets/elements/header.php
+++ assets/elements/header.php
@@ -1,7 +1,46 @@
+<nav class="origin-nav">
+    <ul class="origin-nav-list">
+        <li>
+            <a href="index" class="origin-nav-list-link <?php if($currentPage == 'index') { echo 'origin-nav-list-active'; } ?>">
                 Accueil
+                <?php 
+                    if($currentPage == 'index') {
+                        echo '<div class="active-pin"></div>';
+                    }
+                ?>
+            </a>
+        </li>
+        <li>
+            <a href="lore" class="origin-nav-list-link <?php if($currentPage == 'lore') { echo 'origin-nav-list-active'; } ?>">
                 Lore
+                <?php 
+                    if($currentPage == 'lore') {
+                        echo '<div class="active-pin"></div>';
+                    }
+                ?>
+            </a>
+        </li>
+        <li>
+            <a href="join" class="origin-nav-list-link <?php if($currentPage == 'join') { echo 'origin-nav-list-active'; } ?>">
                 Nous Rejoindre
+                <?php 
+                    if($currentPage == 'join') {
+                        echo '<div class="active-pin"></div>';
+                    }
+                ?>
+            </a>
+        </li>
+        <li>
+            <a href="support" class="origin-nav-list-link origin-nav-list-special group <?php if($currentPage == 'support') { echo 'origin-nav-list-active'; } ?>">
                 Support
+                <?php 
+                    if($currentPage == 'support') {
+                        echo '<div class="active-pin"></div>';
+                    }
+                    else {
+                        echo '<span class="special-pin group-hover:w-4"></span>';
+                    }
+                ?>
             </a>
         </li>
     </ul>
